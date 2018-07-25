a15819fb631a11e2df9d863769d66c55b020fd69
Martina Sundqvist <martina.sundqvist@agroparistech.fr>
2018-07-25 15:27:15 +0200

    functions for clustering

---
+++ R/get_perturbed_data.R
@@ -0,0 +1,10 @@
+
+
+
+randomly_projected_data <- function(data, dimension){}
+
+subsampled_var <- function(data, proportion){data}
+
+# subsampled_obs <- function(data, proportion){}
+
+denoised_data <- function(data, noise) {}
