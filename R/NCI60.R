5e72ecb1c0f8279679499a92fa8cb67bf23f1ac3
Julien Chiquet <julien.chiquet@gmail.com>
2020-07-10 10:37:52 +0200

    adding NCI60 as internal data

---
+++ R/NCI60.R
@@ -0,0 +1,14 @@
+#' NCI60
+#'
+#' Microarray Gene Expression Profiles Of The NCI 60 Cell Lines
+#'
+#' The data contains expression levels on 6830 genes from 64 cancer cell lines. Cancer type is also recorded.
+#'
+#' @format A list with two entries:
+#' \describe{
+#'   \item{expr}{a data.frame with 6830 rows and 64 columns of expression levels}
+#'   \item{type}{a character vector containing the cancer types.}
+#'   ...
+#' }
+#' @source \url{http://genome-www.stanford.edu/nci60/}
+"NCI60"
